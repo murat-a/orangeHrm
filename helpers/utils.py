99d9230544f35f258ce02ef541fa7770f3ff79bc
bersh92@gmail.com <Gu8adaeMo1991>
2024-03-02 15:53:38 +0100

    edit

--- helpers/utils.py
+++ helpers/utils.py
@@ -10,21 +10,7 @@ class Utils:
         self.step = self.app.step
         self.wd = self.app.wd
 
-    BASEDIR = os.path.join(os.getcwd(), "screenshots/")
-
     def generate_random_email(domain='example.com', length=15):
         characters = string.ascii_letters + string.digits
         local_part = ''.join(random.choice(characters) for i in range(length))
         return f"{local_part}@{domain}"
-
-    def takeScreenshot(self):
-        shutil.rmtree(self.BASEDIR)
-        os.makedirs(self.BASEDIR)
-        self.wd.save_screenshot(self.BASEDIR + "test.png")
-
-    def deleteAllScreenshots(self):
-        shutil.rmtree(self.BASEDIR)
-        os.makedirs(self.BASEDIR)
-
-    def getPathToScreenshot(self):
-        return self.BASEDIR + "test.png"
