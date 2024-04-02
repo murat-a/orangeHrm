9e947c7b935f722372d7ac045798dafa17202bfb
bersh92 <32264856+bersh92@users.noreply.github.com>
2024-04-02 15:18:01 +0200

    test conflict resolving

--- test/test_case_commit_conflicts.py
+++
@@ -1,6 +0,0 @@
-
-def test_case_1_login_to_the_application(app):
-    app.orangeHrm.openUrl()
-    app.orangeHrm.login_to_the_application()
-    app.assert_that(app.orangeHrm.get_header_text()).is_equal_to('Employee Management')
-gfhgf
