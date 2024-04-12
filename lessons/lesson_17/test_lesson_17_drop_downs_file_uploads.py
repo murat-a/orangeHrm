d6b464ec289356027f5c8c3501b81bdcdd280039
bersh92 <32264856+bersh92@users.noreply.github.com>
2024-04-12 20:39:48 +0300

    add new lesson 17 class work

--- lessons/lesson_17/test_lesson_17_drop_downs_file_uploads.py
+++ lessons/lesson_17/test_lesson_17_drop_downs_file_uploads.py
@@ -1,3 +1,5 @@
+import time
+
 
 def test_lesson_17_drag_and_drop(app):
     app.demonstrationAppDemoQa.openUrl()
@@ -13,3 +15,20 @@ def test_lesson_17_upload_file(app):
     app.demonstrationAppDemoQa.openUrl()
     app.demonstrationAppDemoQa.go_to_side_menu_section("Elements", "Upload and Download")
     app.demonstrationAppDemoQa.upload_file('/Users/admin/PycharmProjects/orangeHrm/files/test_upload.txt')
+
+#-----------------------------------------------------------------------------------------
+
+def test_lesson_17_test_drag_and_drop(app):
+    app.demonstrationAppDemoQa.openUrl()
+    # go to "Interactions" "Dragabble" section
+    # drag and drop the element to any position from the default from the page
+
+def test_lesson_17_test_drop_down_select(app):
+    app.demonstrationAppDemoQa.openUrl()
+    app.demonstrationAppDemoQa.go_to_side_menu_section("Widgets", "Select Menu")
+    # select any other option from "Select One" drop-down
+
+def test_lesson_17_test_upload_file(app):
+    app.demonstrationAppDemoQa.openUrl()
+    app.demonstrationAppDemoQa.go_to_side_menu_section("Elements", "Upload and Download")
+    # Create some file in your project and upload it using upload method from previous example
