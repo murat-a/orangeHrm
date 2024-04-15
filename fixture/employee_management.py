9223de600a746f68186bdc086cff445499b47a92
mmazb3 <murat.abetayev@gmail.com>
2024-04-15 12:38:52 -0700

    Test case 9 completed

--- fixture/employee_management.py
+++ fixture/employee_management.py
@@ -2,6 +2,9 @@
 from fixture.step import StepHelper
 from selenium.webdriver.remote.webdriver import WebDriver
 
+from fixture.table import Table
+
+
 class EmployeeManagement:
     home_button = "a[data-automation-id='menu_home']"
     list_of_widgets_header_texts = ".widget-header span:last-child"
@@ -9,6 +12,12 @@ class EmployeeManagement:
     def __init__(self, step: StepHelper, wd: WebDriver):
         self.step = step
         self.wd = wd
+        self.table = Table(step,
+                           row_selector='#employeeListTable tbody tr',
+                           column_selectors={'employee_id': 'td:nth-child(2)',
+                                             'employee_name': 'td:nth-child(3)',
+                                             'job_title': 'td:nth-child(4)',
+                                             'employment_status': 'td:nth-child(5)'})
 
     def click_home(self):
         self.step.click_on_element(self.home_button)
