8c1ec0d68ad1976e2b2de71d9544ecc1d8b267e9
bersh92 <32264856+bersh92@users.noreply.github.com>
2024-04-26 20:38:33 +0300

    Lesson 23

--- test/test_case_10_Reports_and_Analytics.py
+++ test/test_case_10_Reports_and_Analytics.py
@@ -1,7 +1,7 @@
 # Test case 10 - verify folders creation functionality
 # TODO: Open the application
 # TODO: Login
-# TODO: Click on Reports and Analysis
+# TODO: Click on Reports and Analytics
 # TODO: Click on New Folder
 # TODO: Fill in the folder name
 # TODO: Click Save
