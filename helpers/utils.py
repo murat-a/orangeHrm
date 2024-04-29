9b8b6ba8c29bed1c7ac16eaf6b8410427bb26bab
bersh92 <32264856+bersh92@users.noreply.github.com>
2024-04-29 14:24:42 +0300

    update utils.py

--- helpers/utils.py
+++ helpers/utils.py
@@ -5,12 +5,14 @@ import string
 
 class Utils:
 
-    def __init__(self, app):
-        self.app = app
-        self.step = self.app.step
-        self.wd = self.app.wd
-
+    @staticmethod
     def generate_random_email(domain='example.com', length=15):
         characters = string.ascii_letters + string.digits
         local_part = ''.join(random.choice(characters) for i in range(length))
         return f"{local_part}@{domain}"
+
+    @staticmethod
+    def generate_random_string(length=15):
+        characters = string.ascii_letters + string.digits
+        random_string = ''.join(random.choice(characters) for i in range(length))
+        return random_string
